837414f104dc61bc2ad83462d9fc2559b20d84d7
lukasz karluk <julapy@googlemail.com>
2016-10-12 11:38:06 +1100

    cocImage API

---
+++ src/image/cocImage.cpp
@@ -0,0 +1,160 @@
+/**
+ *
+ *      ┌─┐╔═╗┌┬┐┌─┐
+ *      │  ║ ║ ││├┤
+ *      └─┘╚═╝─┴┘└─┘
+ *   ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+ *   │  ├─┤║║║└┐┌┘├─┤└─┐
+ *   └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+ *
+ * Copyright (c) 2014-2016 Code on Canvas Pty Ltd, http://CodeOnCanvas.cc
+ *
+ * This software is distributed under the MIT license
+ * https://tldrlegal.com/license/mit-license
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ *
+ **/
+
+#include "cocImage.h"
+
+namespace coc {
+
+//--------------------------------------------------------------
+Image::Image(const glm::vec2 & size) :
+sourceSize(size),
+targetSize(size),
+targetPos(0, 0),
+bTargetChanged(true),
+scaleMode(ScaleMode::None),
+bScaleModeChanged(false),
+insetUpperLeft(0, 0),
+insetLowerRight(size),
+bInsetChanged(false) {
+    //
+}
+
+Image::~Image() {
+
+}
+
+//--------------------------------------------------------------
+ImageRef Image::create(const glm::vec2 & size) {
+    return ImageRef(new Image(size));
+}
+
+//--------------------------------------------------------------
+void Image::setPos(const glm::vec2 & value) {
+    bTargetChanged = bTargetChanged || (targetPos != value);
+    targetPos = value;
+}
+
+const glm::vec2 & Image::getPos() const {
+    return targetPos;
+}
+
+//--------------------------------------------------------------
+void Image::setSize(const glm::vec2 & value) {
+    bTargetChanged = bTargetChanged || (targetSize != value);
+    targetSize = value;
+}
+
+const glm::vec2 & Image::getSize() const {
+    return targetSize;
+}
+
+//--------------------------------------------------------------
+void Image::setRect(const coc::Rect & value) {
+    setPos( glm::vec2(value.getX(), value.getY()) );
+    setSize( glm::vec2(value.getW(), value.getH()) );
+}
+
+coc::Rect Image::getRect() const {
+    return coc::Rect(targetPos.x, targetPos.y, targetPos.x + targetSize.x, targetPos.y + targetSize.y);
+}
+
+//--------------------------------------------------------------
+void Image::setScaleMode(ScaleMode value) {
+    bScaleModeChanged = bScaleModeChanged || (scaleMode != value);
+    scaleMode = value;
+}
+
+Image::ScaleMode Image::getScaleMode() const {
+    return scaleMode;
+}
+
+//--------------------------------------------------------------
+void Image::setInsetUpperLeft(const glm::vec2 & value) {
+    setInsetLeft( value.x );
+    setInsetUpper( value.y );
+}
+
+void Image::setInsetLowerRight(const glm::vec2 & value) {
+    setInsetRight( value.x );
+    setInsetLower( value.y );
+}
+
+void Image::setInsetRect(const coc::Rect & value) {
+    setInsetLeft( value.getX() );
+    setInsetUpper( value.getY() );
+    setInsetRight( sourceSize.x - value.getX() - value.getW() );
+    setInsetLower( sourceSize.y - value.getY() - value.getH() );
+}
+
+void Image::setInset(float value) {
+    setInsetLeft( value );
+    setInsetUpper( value );
+    setInsetRight( value );
+    setInsetLower( value );
+}
+
+void Image::setInsetLeft(float value) {
+    bInsetChanged = bInsetChanged || (insetUpperLeft.x != value);
+    insetUpperLeft.x = value;
+}
+
+void Image::setInsetUpper(float value) {
+    bInsetChanged = bInsetChanged || (insetUpperLeft.y != value);
+    insetUpperLeft.y = value;
+}
+
+void Image::setInsetRight(float value) {
+    bInsetChanged = bInsetChanged || (insetLowerRight.x != value);
+    insetLowerRight.x = value;
+}
+
+void Image::setInsetLower(float value) {
+    bInsetChanged = bInsetChanged || (insetLowerRight.y != value);
+    insetLowerRight.y = value;
+}
+
+//--------------------------------------------------------------    
+void Image::update() {
+    
+    bool bUpdate = false;
+    bUpdate = bUpdate || bTargetChanged;
+    bUpdate = bUpdate || bScaleModeChanged;
+    bUpdate = bUpdate || bInsetChanged;
+    if(bUpdate == false) {
+        return;
+    }
+    
+    if(bTargetChanged) {
+    
+    }
+    
+    if(bScaleModeChanged) {
+        //
+    }
+    
+    if(bInsetChanged) {
+        //
+    }
+    
+    bTargetChanged = false;
+    bScaleModeChanged = false;
+    bInsetChanged = false;
+}
+
+}
---
+++ src/image/cocImage.h
@@ -0,0 +1,96 @@
+/**
+ *
+ *      ┌─┐╔═╗┌┬┐┌─┐
+ *      │  ║ ║ ││├┤
+ *      └─┘╚═╝─┴┘└─┘
+ *   ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+ *   │  ├─┤║║║└┐┌┘├─┤└─┐
+ *   └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+ *
+ * Copyright (c) 2014-2016 Code on Canvas Pty Ltd, http://CodeOnCanvas.cc
+ *
+ * This software is distributed under the MIT license
+ * https://tldrlegal.com/license/mit-license
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ *
+ **/
+
+#pragma once
+
+#include "cocCore.h"
+
+namespace coc {
+
+//--------------------------------------------------------------
+class Image;
+typedef std::shared_ptr<Image> ImageRef;
+
+class Image {
+
+public:
+
+    enum ScaleMode {
+        None=0,
+        Fit,
+        Fill
+    };
+    
+    class Shape {
+    public:
+        std::vector<glm::vec2> verts;
+        std::vector<glm::vec2> texcords;
+    };
+
+    Image(const glm::vec2 & size);
+    ~Image();
+    
+    static ImageRef create(const glm::vec2 & size);
+    
+    void setPos(const glm::vec2 & value);
+    const glm::vec2 & getPos() const;
+    
+    void setSize(const glm::vec2 & value);
+    const glm::vec2 & getSize() const;
+    
+    void setRect(const coc::Rect & value);
+    coc::Rect getRect() const;
+    
+    void setScaleMode(ScaleMode value);
+    ScaleMode getScaleMode() const;
+
+    void setInsetUpperLeft(const glm::vec2 & value);
+    void setInsetLowerRight(const glm::vec2 & value);
+    void setInsetRect(const coc::Rect & value);
+    void setInset(float value);
+    void setInsetLeft(float value);
+    void setInsetUpper(float value);
+    void setInsetRight(float value);
+    void setInsetLower(float value);
+    
+    void update();
+    
+	virtual void draw() const {}
+    virtual void drawDebug() const {}
+
+protected:
+
+    glm::vec2 sourceSize;
+    
+    glm::vec2 targetSize;
+    glm::vec2 targetPos;
+    bool bTargetChanged;
+    
+    ScaleMode scaleMode;
+    bool bScaleModeChanged;
+    
+    glm::vec2 insetUpperLeft;
+    glm::vec2 insetLowerRight;
+    bool bInsetChanged;
+    
+    std::vector<Shape> shapes;
+
+};
+
+}
